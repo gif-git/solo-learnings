4a9e0b753f10e59499d0ba1be2a7ad42003a24af
Giftson Solomon <solomon4usoft@gmail.com>
2020-09-15 13:43:35 +0530

    c++ learning 1st

---
+++ cpp.sh
@@ -0,0 +1,35 @@
+/*
+compiler: human to machine
+        machine to human
+    
+rules: 
+
+1. problem, 
+2. solution - alternative 
+3. program, - C++
+4. compiler,- tutboc, ide, codeblocks, cmd 
+5. result 
+
+reference:-
+library file -
+linker: 
+#include<iostrem> input , output
+std::cout<<"welcome"; standerd character out << " ";
+
+
+*/
+
+int main()
+{
+    std::cout<<"welcome";
+    return 0;
+}
+
+
+/*
+not much importent:-
+Machine language:  101010
+assembly language: mov h1
+
+
+*/
