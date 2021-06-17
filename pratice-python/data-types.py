201870f61de36f99e0f51b9079de2801f64e86cb
gif-git <solomon4usoft@gmail.com>
2021-06-17 19:19:43 +0530

    data type learing

---
+++ pratice-python/data-types.py
@@ -0,0 +1,23 @@
+a=5
+print ('The type of ',a, type(a))
+
+a=4.6
+print ('The type of ',a, type(a))
+
+a=1+2j
+print ('The type of ',a, type(a))
+
+a='Python programming'
+print ('the type of ',a, type(a))
+
+a=[1,'a','python',4.6]
+print ('the type of ',a, type(a))
+
+a=(1,'a','python',4.6]
+print ('The type of ',a, type(a))
+
+a={'name':'aaa', 'dept':'bbb'}
+print ('The type of ',a, type(a))
+
+a={1,2,3}
+print ('The type of ',a, type(a))
